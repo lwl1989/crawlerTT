58cfe3d02d3d5428b5a443dfc6a67a876d5a791e
li <285753421@qq.com>
2018-05-10 17:38:21 +0800

    path

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import "github.com/lwl1989/crawlerTT/user"
+import (
+	"github.com/lwl1989/crawlerTT/user"
+	"fmt"
+)
 
 func main()  {
-	user.GetConfig("/www/go_path/src/github.com/lwl1989/crawlerTT/config.json")
+	path := ""
+	fmt.Println("請輸入配置文件(默認當前目錄下config.json)")
+	fmt.Scanln(&path)
+	//"/www/go_path/src/github.com/lwl1989/crawlerTT/config.json"
+	user.GetConfig(path)
 	user.GetList()
 	//user.GetUserInfo("MkigJdpKFSkE6ZC")
 }
